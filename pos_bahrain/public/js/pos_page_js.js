ebc54fcd70d42e3d8438a5ff236b83d598f34bd5
Sun Howwrongbum <sun@libermatic.com>
2018-12-19 22:43:28 +0530

    POS: set focus to search field after batch select

--- pos_bahrain/public/js/pos_page_js.js
+++ pos_bahrain/public/js/pos_page_js.js
@@ -73,6 +73,7 @@ erpnext.pos.PointOfSale = erpnext.pos.PointOfSale.extend({
       this.batch_dialog.set_primary_action(__('Submit'), () => {
         this.item_batch_no[item_code] = this.batch_dialog.get_value('batch');
         this.batch_dialog.hide();
+        this.set_focus();
       });
       this.batch_dialog.get_close_btn().on('click', () => {
         this.item_code = item_code;
@@ -80,6 +81,7 @@ erpnext.pos.PointOfSale = erpnext.pos.PointOfSale.extend({
         this.remove_selected_item();
         this.wrapper.find('.selected-item').empty();
         this.item_code = null;
+        this.set_focus();
       });
       this.batch_dialog.show();
       this.batch_dialog.$wrapper.find('.modal-backdrop').off('click');
