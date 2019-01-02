465f34fb4dcb6e8019261bdc71c3fa5cf1e2c2ea
Sun Howwrongbum <sun@libermatic.com>
2019-01-03 01:26:35 +0530

    POS Bahrain Settings: add field to handle defaults

--- pos_bahrain/pos_bahrain/doctype/pos_bahrain_settings/pos_bahrain_settings.js
+++ pos_bahrain/pos_bahrain/doctype/pos_bahrain_settings/pos_bahrain_settings.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('POS Bahrain Settings', {
-	refresh: function(frm) {
-
-	}
+  onload: function(frm) {
+    frm.trigger('handle_price_list_fields');
+  },
+  discount_on_retail: function(frm) {
+    frm.trigger('handle_price_list_fields');
+  },
+  handle_price_list_fields: function(frm) {
+    const { discount_on_retail } = frm.doc;
+    frm.toggle_reqd(
+      ['retail_price_list', 'wholesale_price_list'],
+      discount_on_retail
+    );
+  },
 });
