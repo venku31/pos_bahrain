890da698dd6c643edbb1e3d35f02591fd6de56bf
Sun Howwrongbum <sun@libermatic.com>
2019-05-10 01:15:57 +0530

    POS: add more keyboard shortcuts

--- pos_bahrain/public/js/pos_page_js.js
+++ pos_bahrain/public/js/pos_page_js.js
@@ -382,15 +382,29 @@ erpnext.pos.PointOfSale = erpnext.pos.PointOfSale.extend({
   },
   bind_keyboard_shortcuts: function() {
     $(document).on('keydown', e => {
-      if (this.numeric_keypad && e.keyCode === 120) {
-        e.preventDefault();
-        e.stopPropagation();
-        if (this.dialog && this.dialog.is_visible) {
-          this.dialog.hide();
-        } else {
-          $(this.numeric_keypad)
-            .find('.pos-pay')
-            .trigger('click');
+      if (frappe.get_route_str() === 'pos') {
+        if (this.numeric_keypad && e.keyCode === 120) {
+          e.preventDefault();
+          e.stopPropagation();
+          if (this.dialog && this.dialog.is_visible) {
+            this.dialog.hide();
+          } else {
+            $(this.numeric_keypad)
+              .find('.pos-pay')
+              .trigger('click');
+          }
+        } else if (
+          this.frm.doc.docstatus == 1 &&
+          e.ctrlKey &&
+          e.keyCode === 80
+        ) {
+          e.preventDefault();
+          e.stopPropagation();
+          this.page.btn_secondary.trigger('click');
+        } else if (e.ctrlKey && e.keyCode === 66) {
+          e.preventDefault();
+          e.stopPropagation();
+          this.page.btn_primary.trigger('click');
         }
       }
     });
