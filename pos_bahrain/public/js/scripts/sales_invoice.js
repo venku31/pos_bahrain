2b5333f33abd9267e86028f45bcb99eb8d3d0e62
Sun Howwrongbum <sun@libermatic.com>
2019-06-20 01:58:39 +0530

    feat(Sales Invoice): use Settings to set Batch price

--- pos_bahrain/public/js/scripts/sales_invoice.js
+++ pos_bahrain/public/js/scripts/sales_invoice.js
@@ -1,22 +1,30 @@
 async function set_rate_from_batch(frm, cdt, cdn) {
   const { batch_no, conversion_factor = 1 } = frappe.get_doc(cdt, cdn) || {};
-  if (batch_no) {
-    const {
-      message: {
-        pb_price_based_on: based_on,
-        pb_rate: rate,
-        pb_discount: discount_percentage,
-      } = {},
-    } = await frappe.db.get_value('Batch', batch_no, [
-      'pb_price_based_on',
-      'pb_rate',
-      'pb_discount',
-    ]);
-    if (based_on === 'Based on Rate') {
-      frappe.model.set_value(cdt, cdn, { rate: rate * conversion_factor });
-    } else if (based_on === 'Based on Discount') {
-      frappe.model.set_value(cdt, cdn, { discount_percentage });
-    }
+  if (!batch_no) {
+    return;
+  }
+  const use_batch_price = await frappe.db.get_single_value(
+    'POS Bahrain Settings',
+    'use_batch_price'
+  );
+  if (!use_batch_price) {
+    return;
+  }
+  const {
+    message: {
+      pb_price_based_on: based_on,
+      pb_rate: rate,
+      pb_discount: discount_percentage,
+    } = {},
+  } = await frappe.db.get_value('Batch', batch_no, [
+    'pb_price_based_on',
+    'pb_rate',
+    'pb_discount',
+  ]);
+  if (based_on === 'Based on Rate') {
+    frappe.model.set_value(cdt, cdn, { rate: rate * conversion_factor });
+  } else if (based_on === 'Based on Discount') {
+    frappe.model.set_value(cdt, cdn, { discount_percentage });
   }
 }
 
