3c0a489299fcb39d85f54dd38557b01364d9af7d
Sun Howwrongbum <sun@libermatic.com>
2019-06-20 01:57:40 +0530

    feat(POS Bahrain Settings): show/hide Batch custom fields

--- pos_bahrain/pos_bahrain/doctype/pos_bahrain_settings/pos_bahrain_settings.py
+++ pos_bahrain/pos_bahrain/doctype/pos_bahrain_settings/pos_bahrain_settings.py
@@ -3,8 +3,16 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+from frappe.utils import cint
 from frappe.model.document import Document
+from frappe.custom.doctype.property_setter.property_setter import make_property_setter
+
 
 class POSBahrainSettings(Document):
-	pass
+    def on_update(self):
+        toggle_fields(not cint(self.use_batch_price))
+
+
+def toggle_fields(hide):
+    make_property_setter("Batch", "pb_price_sec", "hidden", hide, "Check")
+    make_property_setter("Batch", "pb_price_sec", "print_hide", hide, "Check")
