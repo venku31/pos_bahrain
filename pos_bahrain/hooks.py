b9dcb8572bfcb99ea10728c1e16f0343019fd7e4
Sun Howwrongbum <sun@libermatic.com>
2019-06-20 16:47:05 +0530

    feat(Item): add uom custom field to Item Barcode

--- pos_bahrain/hooks.py
+++ pos_bahrain/hooks.py
@@ -84,6 +84,7 @@ fixtures = [
                     "Batch-pb_rate",
                     "Batch-pb_discount",
                     "Sales Order Item-batch_no",
+                    "Item Barcode-pb_uom",
                 ],
             ]
         ],
