18b1468879a3576ef351597829431ff4789d1839
Sun Howwrongbum <sun@libermatic.com>
2019-06-20 20:50:56 +0530

    feat(POS): set uom to invoice items from barcode

--- pos_bahrain/public/js/addons/withBarcodeUom.js
+++ pos_bahrain/public/js/addons/withBarcodeUom.js
@@ -1,3 +1,5 @@
+import first from 'lodash/first';
+
 export default function withBarcodeUom(Pos) {
   return class PosExtended extends Pos {
     async init_master_data(r, freeze) {
@@ -5,7 +7,37 @@ export default function withBarcodeUom(Pos) {
       const { use_barcode_uom, barcode_details } = pos_data;
       this.use_barcode_uom = !!cint(use_barcode_uom);
       this.barcode_details = barcode_details;
+      this.barcode = null;
       return pos_data;
+    }
+    make_item_list() {
+      this.barcode =
+        this.use_barcode_uom &&
+        this.barcode_details &&
+        this.barcode_details[this.search_item.$input.val()];
+      super.make_item_list();
+    }
+    _set_item_uom(item_code, uom, barcode = null) {
+      const item = this.frm.doc.items.find(x => x.item_code === item_code);
+      if (item && uom) {
+        const { conversion_factor = 1 } = (
+          this.uom_details[item_code] || []
+        ).find(x => x.uom === uom);
+        Object.assign(item, { barcode, uom, conversion_factor });
+      }
+    }
+    add_to_cart() {
+      super.add_to_cart();
+      if (this.use_barcode_uom) {
+        if (this.barcode) {
+          const { item_code, uom, barcode } = this.barcode;
+          this._set_item_uom(item_code, uom, barcode);
+          this.barcode = null;
+        } else {
+          const { item_code, stock_uom: uom } = first(this.items) || {};
+          this._set_item_uom(item_code, uom);
+        }
+      }
     }
   };
 }
