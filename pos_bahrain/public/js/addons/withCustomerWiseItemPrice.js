df6611dc044cd3f0c75c9ee7b7f73555dec92d4e
Sun Howwrongbum <sun@libermatic.com>
2019-07-28 04:06:33 +0530

    feat(pos): handle customer-wise Item Price

---
+++ pos_bahrain/public/js/addons/withCustomerWiseItemPrice.js
@@ -0,0 +1,30 @@
+import mapValues from 'lodash/mapValues';
+import groupBy from 'lodash/groupBy';
+import get from 'lodash/get';
+
+export default function withCustomerWiseItemPrice(Pos) {
+  return class PosExtended extends Pos {
+    async init_master_data(r, freeze) {
+      const pos_data = await super.init_master_data(r, freeze);
+      const { item_prices } = pos_data;
+      this.item_prices_by_customer = mapValues(item_prices, values =>
+        groupBy(values, 'customer')
+      );
+      return pos_data;
+    }
+    get_item_price(item_code, uom_details) {
+      const customer_prices = get(this.item_prices_by_customer, [
+        item_code,
+        this.frm.doc.customer,
+      ]);
+      if (customer_prices && customer_prices.length > 0) {
+        const { uom, conversion_factor = 1 } = uom_details;
+        const customer_wise = customer_prices.find(price => price.uom === uom);
+        if (customer_wise) {
+          return customer_wise.price_list_rate;
+        }
+      }
+      return super.get_item_price(item_code, uom_details);
+    }
+  };
+}
