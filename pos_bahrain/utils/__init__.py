572df8cff54d27be570f31306792ed3ba08ad0fd
Sun Howwrongbum <sun@libermatic.com>
2019-07-28 02:32:03 +0530

    feat(utils): add report error handler

--- pos_bahrain/utils/__init__.py
+++ pos_bahrain/utils/__init__.py
@@ -1,5 +1,6 @@
 from functools import partial
 from toolz import keyfilter, compose, curry
+from pymysql.err import ProgrammingError
 
 
 def pick(whitelist, d):
@@ -9,3 +10,13 @@ def pick(whitelist, d):
 @curry
 def sum_by(key, iterand):
     return compose(sum, partial(map, lambda x: x.get(key)))(iterand)
+
+
+def with_report_error_check(data_fn):
+    def fn(*args, **kwargs):
+        try:
+            return data_fn(*args, **kwargs)
+        except ProgrammingError:
+            return []
+
+    return fn
