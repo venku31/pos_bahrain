c5fb9ebfd3de13bcff6c6dfea516c3c8af65e653
Ivan Ray Altomera <irayspacii@gmail.com>
2019-07-31 21:30:56 +0800

    report: set currency, and set balances

--- pos_bahrain/pos_bahrain/report/cash_account/cash_account.py
+++ pos_bahrain/pos_bahrain/report/cash_account/cash_account.py
@@ -5,15 +5,21 @@ from __future__ import unicode_literals
 import frappe
 
 from frappe import _
+from erpnext import get_company_currency, get_default_company
+
+from toolz import merge
 
 
 def execute(filters=None):
-	columns = _get_columns(filters)
-	data = _get_data()
+	company = get_default_company()
+
+	columns = _get_columns(company)
+	data = _get_data(company)
+
 	return columns, data
 
 
-def _get_columns(filters):
+def _get_columns(company):
 	def make_column(key, label=None, type="Data", options=None, width=120):
 		return {
 			"label": _(label or key.replace("_", " ").title()),
@@ -23,17 +29,31 @@ def _get_columns(filters):
 			"width": width
 		}
 
-	return [
+	currency = get_company_currency(company)
+
+	columns = [
 		make_column("posting_date", "Date", type="Date"),
 		make_column("voucher_type", "Document Type"),
-		make_column("voucher_no", "Document No"),
-		make_column("debit", "Cash In", type="currency"),
-		make_column("credit", "Cash Out", type="currency"),
-		make_column("balance", "Cash In - Cash Out", type="currency")
+		make_column(
+			"voucher_no",
+			"Document No",
+			"Dynamic Link",
+			"voucher_type",
+			180
+		),
 	]
 
+	columns.extend([
+		make_column("debit", "Cash In ({0})".format(currency), type="Float"),
+		make_column("credit", "Cash Out ({0})".format(currency), type="Float"),
+		make_column("balance", "Balance ({0})".format(currency), type="Float")
+	])
 
-def _get_data():
+	return columns
+
+
+def _get_data(company):
+	cash_account = frappe.db.get_value('Company', company, 'default_cash_account')
 	result = frappe.db.sql(
 		"""
 			SELECT
@@ -43,9 +63,28 @@ def _get_data():
 				debit,
 				credit
 			FROM `tabGL Entry`
-			WHERE voucher_type IN ('Sales Invoice', 'Purchase Invoice', 'Payment Entry', 'Journal Entry')
+			WHERE 
+				voucher_type IN ('Sales Invoice', 'Purchase Invoice', 'Payment Entry', 'Journal Entry')
+			AND company=%(company)s AND account=%(account)s
 		""",
+		values={'company': company, 'account': cash_account},
 		as_dict=True
 	)
 
+	result = set_balance(result)
+
 	return result
+
+
+def set_balance(data):
+	data_with_balances = []
+
+	balance = 0.00
+	for row in data:
+		balance = balance + (row.debit - row.credit)
+		data_with_balances.append(
+			merge(row, {'balance': balance})
+		)
+
+	return data_with_balances
+
