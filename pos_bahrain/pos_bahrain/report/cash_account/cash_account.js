9d12ca6798d2c728dd09fc502d3cdc2d3075ebf0
Ivan Ray Altomera <irayspacii@gmail.com>
2019-08-01 13:39:03 +0800

    feat: Cash Account date range filter

--- pos_bahrain/pos_bahrain/report/cash_account/cash_account.js
+++ pos_bahrain/pos_bahrain/report/cash_account/cash_account.js
@@ -2,8 +2,19 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
+
 frappe.query_reports["Cash Account"] = {
 	"filters": [
-
+		_make_filter('from_date', 'From Date', 'Date', frappe.datetime.month_start()),
+		_make_filter('to_date', 'To Date', 'Date', frappe.datetime.month_end())
 	]
+};
+
+function _make_filter(key, label, type, def) {
+	return {
+		fieldname: key,
+		fieldtype: type,
+		label: __(label),
+		default: def
+	};
 }
