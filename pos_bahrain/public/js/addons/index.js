054ddfbb60e2bde84eb3c2a109cd706ff93a0b03
Ivan Ray Altomera <irayspacii@gmail.com>
2019-08-26 16:41:16 +0800

    feat(POS): modifiable batch field

--- pos_bahrain/public/js/addons/index.js
+++ pos_bahrain/public/js/addons/index.js
@@ -5,13 +5,17 @@ import withBatchPrice from './withBatchPrice';
 import withBarcodeUom from './withBarcodeUom';
 import withCustomerWiseItemPrice from './withCustomerWiseItemPrice';
 import withStockValidator from './withStockValidator';
+import withBatchField from './withBatchField';
+import withIdx from './withIdx';
 
 // the order of the hocs is important. `withUom` should always run before all
 // other hocs
 export const extend_pos = flowRight([
+  withIdx,
   withBarcodeUom,
   withBatchPrice,
   withCustomerWiseItemPrice,
   withStockValidator,
+  withBatchField,
   withUom,
 ]);
