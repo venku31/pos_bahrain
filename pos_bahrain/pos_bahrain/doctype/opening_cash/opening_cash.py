9ce603454c8b761ad0592dae9693d35bbe2e3b1b
Ivan Ray Altomera <irayspacii@gmail.com>
2019-08-26 17:40:19 +0800

    refactor(indent): mixed spaces and tabs error

--- pos_bahrain/pos_bahrain/doctype/opening_cash/opening_cash.py
+++ pos_bahrain/pos_bahrain/doctype/opening_cash/opening_cash.py
@@ -6,12 +6,17 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
-class OpeningCash(Document):
-	def validate(self):
-        	opening_voucher = frappe.db.sql("""select name from `tabOpening Cash` where date=%s and pos_profile=%s and docstatus=1""",(self.date,self.pos_profile))
-                if opening_voucher:
-                        pos_closing_voucher = frappe.db.sql("""select name from `tabPOS Closing Voucher` where period_start_date=%s and pos_profile=%s and docstatus=1""",(self.date,self.pos_profile))
-                        if not pos_closing_voucher:
-                                frappe.throw("POS Closing Voucher is not submitted yet")
 
-	
+class OpeningCash(Document):
+    def validate(self):
+        opening_voucher = frappe.db.sql("""
+          SELECT name FROM `tabOpening Cash` 
+          WHERE date=%s AND pos_profile=%s AND docstatus=1
+          """, (self.date, self.pos_profile))
+        if opening_voucher:
+            pos_closing_voucher = frappe.db.sql("""
+              SELECT name FROM `tabPOS Closing Voucher` 
+              WHERE period_start_date=%s AND pos_profile=%s AND docstatus=1
+              """, (self.date, self.pos_profile))
+        if not pos_closing_voucher:
+            frappe.throw("POS Closing Voucher is not submitted yet")
