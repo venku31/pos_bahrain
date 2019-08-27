4c932bd488817e3eb5443904cc8df1096cc2b4bb
Sun Howwrongbum <sun@libermatic.com>
2019-08-27 20:05:23 +0530

    format: descriptive var name

--- pos_bahrain/public/js/addons/withIdx.js
+++ pos_bahrain/public/js/addons/withIdx.js
@@ -5,9 +5,9 @@ export default function withIdx(Pos) {
             super.show_items_in_item_cart();
             this._set_pos_bill_item_idx();
             $(this.wrapper).find('.pos-bill-item').removeClass('active');
-            if (this.cart_idx) {
+            if (this.selected_cart_idx) {
                 $(this.wrapper)
-                    .find(`.pos-bill-item[data-idx="${this.cart_idx}"]`)
+                    .find(`.pos-bill-item[data-idx="${this.selected_cart_idx}"]`)
                     .addClass('active');
             }
         }
@@ -18,19 +18,19 @@ export default function withIdx(Pos) {
             });
         }
         make_new_cart() {
-            this.cart_idx = null;
+            this.selected_cart_idx = null;
             super.make_new_cart();
         }
         bind_events() {
             super.bind_events();
             $(this.wrapper).on('click', '.pos-item-wrapper', e => {
-                this.cart_idx = null;
+                this.selected_cart_idx = null;
             })
         }
         bind_items_event() {
             super.bind_items_event();
             $(this.wrapper).on('click', '.pos-bill-item', e => {
-                this.cart_idx = $(e.currentTarget).attr("data-idx");
+                this.selected_cart_idx = $(e.currentTarget).attr("data-idx");
             })
         }
     };
