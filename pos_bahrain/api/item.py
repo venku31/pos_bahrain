77d1c1285b1ccd071369e81ffcaf5073ce417480
Ivan Ray Altomera <irayspacii@gmail.com>
2019-09-01 15:45:49 +0800

    feat(pos): initial use custom item cart

--- pos_bahrain/api/item.py
+++ pos_bahrain/api/item.py
@@ -28,6 +28,7 @@ def get_more_pos_data(profile, company):
         "do_not_allow_zero_payment": settings.do_not_allow_zero_payment,
         "use_batch_price": settings.use_batch_price,
         "use_barcode_uom": settings.use_barcode_uom,
+        "use_custom_item_cart": settings.use_custom_item_cart
     }
 
 
