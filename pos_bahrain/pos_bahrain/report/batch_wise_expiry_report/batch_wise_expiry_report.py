8643a5f490e042c558d4a8a3cd0f2a6d60213205
Sun Howwrongbum <sun@libermatic.com>
2019-10-21 23:07:10 +0530

    feat(report): hide zero stock in Batch-wise Expiry

--- pos_bahrain/pos_bahrain/report/batch_wise_expiry_report/batch_wise_expiry_report.py
+++ pos_bahrain/pos_bahrain/report/batch_wise_expiry_report/batch_wise_expiry_report.py
@@ -37,9 +37,9 @@ def _get_args(filters={}):
 
 
 def _get_columns(args):
-    def make_column(key, label, type="Currency", options=None, width=120):
+    def make_column(key, label=None, type="Currency", options=None, width=120):
         return {
-            "label": _(label),
+            "label": _(label or key.replace("_", " ").title()),
             "fieldname": key,
             "fieldtype": type,
             "options": options,
@@ -47,12 +47,12 @@ def _get_columns(args):
         }
 
     columns = [
-        make_column("supplier", "Supplier", type="Link", options="Supplier"),
-        make_column("brand", "Brand", type="Link", options="Brand"),
-        make_column("item_code", "Item Code", type="Link", options="Item"),
-        make_column("item_name", "Item Name", type="Data", width=200),
+        make_column("supplier", type="Link", options="Supplier"),
+        make_column("brand", type="Link", options="Brand"),
+        make_column("item_code", type="Link", options="Item"),
+        make_column("item_name", type="Data", width=200),
         make_column("batch_no", "Batch", type="Link", options="Batch"),
-        make_column("expiry_date", "Expiry Date", type="Date", width=90),
+        make_column("expiry_date", type="Date", width=90),
         make_column("expiry_in_days", "Expiry in Days", type="Int", width=90),
         make_column("qty", "Quantity", type="Float", width=90),
         make_column("price1", args.get("price_list1")),
@@ -110,4 +110,12 @@ def _get_data(args, keys):
 
     make_row = compose(partial(keyfilter, lambda k: k in keys), set_expiry)
 
-    return map(make_row, sles)
+    filter_rows = compose(
+        list,
+        partial(
+            filter, lambda x: True if not args.get("hide_zero_stock") else x.get("qty")
+        ),
+        partial(map, make_row),
+    )
+
+    return filter_rows(sles)
