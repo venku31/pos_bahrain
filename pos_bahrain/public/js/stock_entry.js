bc0f5d7187d587f273da279dad772a0236afa622
Sun Howwrongbum <sun@libermatic.com>
2019-12-17 01:01:44 +0530

    fix(Stock Entry): ignore batch selector when scan_barcode by batch_no

--- pos_bahrain/public/js/stock_entry.js
+++ pos_bahrain/public/js/stock_entry.js
@@ -1,52 +1,16 @@
 // Copyright (c) 2018, 	9t9it and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.off(cur_frm.doctype, 'scan_barcode');
+frappe.ui.form.off('Stock Entry', 'scan_barcode');
 frappe.ui.form.on(
-  cur_frm.doctype,
+  'Stock Entry',
   'scan_barcode',
   pos_bahrain.scripts.extensions.scan_barcode
 );
 
-const select_batch_and_serial_no = erpnext.stock.select_batch_and_serial_no;
-
-erpnext.stock.select_batch_and_serial_no = (frm, item) => {
-  if (item && item.has_batch_no && frm.doc.purpose === 'Material Receipt') {
-    frappe.require(
-      'assets/erpnext/js/utils/serial_no_batch_selector.js',
-      function() {
-        const SerialNoBatchSelector = erpnext.SerialNoBatchSelector.extend({
-          get_batch_fields: function() {
-            const fields = this._super();
-            if (fields) {
-              const batches = fields.find(
-                ({ fieldname }) => fieldname === 'batches'
-              );
-              if (batches) {
-                const batch_no = batches.fields.find(
-                  ({ fieldname }) => fieldname === 'batch_no'
-                );
-                if (batch_no) {
-                  batch_no.get_query = () => ({
-                    filters: { item: this.item_code },
-                  });
-                }
-              }
-            }
-            return fields;
-          },
-        });
-        new SerialNoBatchSelector({
-          frm: frm,
-          item: item,
-          warehouse_details: {
-            type: 'Target Warehouse',
-            name: cstr(item.t_warehouse) || '',
-          },
-        });
-      }
-    );
-  } else {
-    select_batch_and_serial_no(frm, item);
-  }
-};
+frappe.ui.form.off('Stock Entry Detail', 'item_code');
+frappe.ui.form.on(
+  'Stock Entry Detail',
+  'item_code',
+  pos_bahrain.scripts.extensions.stock_entry_item_code
+);
