40682a874605e84dedb47902cda73f1a765278f9
Sun Howwrongbum <sun@libermatic.com>
2019-12-19 23:48:53 +0530

    feat(pos): make change amount field read only

--- pos_bahrain/public/js/addons/withModifiedPaymentDialogFields.js
+++ pos_bahrain/public/js/addons/withModifiedPaymentDialogFields.js
@@ -5,13 +5,30 @@ export default function withModifiedPaymentDialogFields(Pos) {
         this.dialog.$wrapper.remove();
       }
       super.make_payment();
-      ['.write_off_amount'].forEach(selector => {
+      ['.change_amount', '.write_off_amount'].forEach(selector => {
         this.dialog.$body
           .find(selector)
           .parent()
           .parent()
           .addClass('hidden');
       });
+      $(
+        `<div class="col-xs-6 col-sm-3 text-center">
+          <p style="font-size: 16px;">Change</p>
+          <h3 class="change_amount_static">${format_currency(
+            this.frm.doc.change_amount,
+            this.frm.doc.currency
+          )}</h3>
+        </div>`
+      ).appendTo(this.dialog.$body.find('.amount-row'));
+    }
+    show_amounts() {
+      super.show_amounts();
+      $(this.$body)
+        .find('.change_amount_static')
+        .text(
+          format_currency(this.frm.doc.change_amount, this.frm.doc.currency)
+        );
     }
   };
 }
