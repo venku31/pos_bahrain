461a874d9f13433eeb2c05d1fb2520c138d7eeb0
Sun Howwrongbum <sun@libermatic.com>
2019-12-25 21:06:46 +0530

    fix(pos): use new price getter method

--- pos_bahrain/public/js/addons/withCustomerWiseItemPrice.js
+++ pos_bahrain/public/js/addons/withCustomerWiseItemPrice.js
@@ -4,15 +4,12 @@ import get from 'lodash/get';
 import keyBy from 'lodash/keyBy';
 import merge from 'lodash/merge';
 
-// depends on withUom
 export default function withCustomerWiseItemPrice(Pos) {
   return class PosExtended extends Pos {
     async init_master_data(r, freeze) {
       const pos_data = await super.init_master_data(r, freeze);
       const { item_prices } = pos_data;
-      this.item_prices_by_customer = mapValues(item_prices, values =>
-        groupBy(values.filter(({ customer }) => customer), 'customer')
-      );
+
       const customer_wise_price_list = mapValues(
         groupBy(
           Object.values(item_prices)
@@ -32,19 +29,6 @@ export default function withCustomerWiseItemPrice(Pos) {
       );
       this.make_item_list(this.default_customer);
       return pos_data;
-    }
-    get_item_price({ item_code, uom }) {
-      const customer_prices = get(this.item_prices_by_customer, [
-        item_code,
-        this.frm.doc.customer,
-      ]);
-      if (customer_prices && customer_prices.length > 0) {
-        const customer_wise = customer_prices.find(price => price.uom === uom);
-        if (customer_wise) {
-          return customer_wise.price_list_rate;
-        }
-      }
-      return super.get_item_price({ item_code, uom });
     }
   };
 }
--- pos_bahrain/public/js/addons/withUom.js
+++ pos_bahrain/public/js/addons/withUom.js
@@ -3,73 +3,23 @@ import mapValues from 'lodash/mapValues';
 import keyBy from 'lodash/keyBy';
 import get from 'lodash/get';
 
-// depends on withIdx
+// depends on withIdx, withExtendedItemPrice
 export default function withUom(Pos) {
   return class PosExtended extends Pos {
-    async init_master_data(r, freeze) {
-      const pos_data = await super.init_master_data(r, freeze);
-      const { uom_details, item_prices } = pos_data;
-      if (!uom_details) {
-        frappe.msgprint({
-          indicator: 'orange',
-          title: __('Warning'),
-          message: __('Unable to load UOM details. Usage will be restricted.'),
-        });
-      }
-      this.uom_details = uom_details;
-      this.item_prices_by_uom = mapValues(item_prices, values =>
-        keyBy(values, 'uom')
-      );
-
-      return pos_data;
-    }
-    add_new_item_to_grid() {
-      super.add_new_item_to_grid();
-      const { item_code, stock_uom: uom } = this.child;
-      const rate = this.get_item_price({ item_code, uom });
-      Object.assign(this.child, {
-        uom,
-        rate,
-        price_list_rate: rate,
-        conversion_factor: 1,
-      });
-    }
-    get_item_price({ item_code, uom }) {
-      const { conversion_factor = 1 } =
-        (this.uom_details[item_code] || []).find(x => x.uom === uom) || {};
-
-      const customer_wise = get(this.customer_wise_price_list, [
-        this.frm.doc.customer,
-        item_code,
-      ]);
-      if (customer_wise) {
-        return flt(
-          (customer_wise * conversion_factor) / this.frm.doc.conversion_rate,
-          this.precision
-        );
-      }
-
-      const uom_wise = get(this.item_prices_by_uom, [
-        item_code,
-        uom,
-        'price_list_rate',
-      ]);
-      if (uom_wise) {
-        return uom_wise;
-      }
-
-      return flt(
-        (get(this.price_list_data, item_code, 0) * conversion_factor) /
-          this.frm.doc.conversion_rate,
-        this.precision
-      );
-    }
     _set_item_price_from_uom(item_code, uom) {
       const item = this._get_active_item_ref_from_doc();
       const uom_details = this.uom_details[item_code].find(x => x.uom === uom);
       if (item && uom_details) {
         const { conversion_factor = 1 } = uom_details;
-        const price_list_rate = this.get_item_price({ item_code, uom });
+        const { customer, posting_date: transaction_date } = this.frm.doc;
+        const { qty: min_qty } = item;
+        const price_list_rate = this.get_item_price({
+          item_code,
+          uom,
+          customer,
+          min_qty,
+          transaction_date,
+        });
         Object.assign(item, {
           uom,
           conversion_factor,
