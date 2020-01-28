7ca159f49f645506368642a180ca241a5b495a83
Sun Howwrongbum <sun@libermatic.com>
2020-01-28 18:25:11 +0530

    feat(report): copy accounts receivable

---
+++ pos_bahrain/pos_bahrain/report/accounts_receivable_2/accounts_receivable_2.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2013, 	9t9it and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from erpnext.accounts.report.accounts_receivable.accounts_receivable import (
+    execute as accounts_receivable,
+)
+
+
+def execute(filters=None):
+    return accounts_receivable(filters)
