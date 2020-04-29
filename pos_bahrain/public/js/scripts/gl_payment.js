953d9843c8cf7256788044db86cee23dc629099d
Sun Howwrongbum <sun@libermatic.com>
2020-04-29 17:31:12 +0000

    feat(GL Paymet): setup queries

--- pos_bahrain/public/js/scripts/gl_payment.js
+++ pos_bahrain/public/js/scripts/gl_payment.js
@@ -1,4 +1,31 @@
-function setup_queries(frm) {}
+function setup_queries(frm) {
+  frm.set_query('cost_center', function ({ company }) {
+    return { filters: { company, is_group: 0 } };
+  });
+  frm.set_query('party_type', function () {
+    return {
+      filters: {
+        name: ['in', Object.keys(frappe.boot.party_account_types)],
+      },
+    };
+  });
+  frm.set_query('account', 'items', function ({ company }) {
+    return { filters: { company, root_type: ['in', ['Income', 'Expense']] } };
+  });
+  frm.set_query('template_type', 'items', function () {
+    return {
+      filters: {
+        name: [
+          'in',
+          [
+            'Sales Taxes and Charges Template',
+            'Purchase Taxes and Charges Template',
+          ],
+        ],
+      },
+    };
+  });
+}
 
 export default function () {
   return {
