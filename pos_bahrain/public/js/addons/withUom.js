5a56c28aaee8f624480bfa8651eae44b016f24fc
Ivan Ray Altomera <irayspacii@gmail.com>
2020-10-23 22:21:49 +0800

    fix(withUom): unescape item_code

--- pos_bahrain/public/js/addons/withUom.js
+++ pos_bahrain/public/js/addons/withUom.js
@@ -43,7 +43,7 @@ export default function withUom(Pos) {
       `).prependTo(this.wrapper.find('.pos-selected-item-action'));
       const $select = this.wrapper.find('.pos-item-uom');
       const selected_item = this._get_active_item_ref_from_doc();
-      this.uom_details[this.item_code].forEach(({ uom }) => {
+      this.uom_details[unescape(this.item_code)].forEach(({ uom }) => {
         $('<option />', {
           value: uom,
           selected: selected_item && uom === selected_item.uom,
