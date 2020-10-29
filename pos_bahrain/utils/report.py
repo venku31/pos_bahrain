054da8c5e9c9c940bdc2996ef7ca9d970ae0c422
Ivan Ray Altomera <irayspacii@gmail.com>
2020-10-29 17:24:00 +0800

    feat(utils): report make_period_list

--- pos_bahrain/utils/report.py
+++ pos_bahrain/utils/report.py
@@ -9,3 +9,33 @@ def make_column(key, label=None, type="Data", width=120, **kwargs):
         "fieldtype": type,
         "width": width,
     }, kwargs)
+
+
+def make_period_list(start_date, end_date):
+    def make_date(data):
+        current_date = start_date + relativedelta.relativedelta(months=data)
+        x, last_day = calendar.monthrange(current_date.year, current_date.month)
+        return {
+            'start_date': current_date.replace(day=1) if start_date.month != current_date.month else start_date,
+            'end_date': current_date.replace(day=last_day) if end_date.month != current_date.month else end_date
+        }
+
+    def make_data(data):
+        month = data.get("start_date").strftime("%b")
+        year = data.get("start_date").strftime("%Y")
+        return frappe._dict({
+            'from_date': data.get('start_date'),
+            'to_date': data.get('end_date'),
+            'year_start_date': data.get('start_date'),
+            'year_end_date': data.get('end_date'),
+            'key': f'{month.lower()}_{year}',
+            'label': f'{month} {year}'
+        })
+
+    start_date = getdate(start_date)
+    end_date = getdate(end_date)
+    if start_date > end_date:
+        frappe.throw(_('Please set start date before the end date'))
+    r = relativedelta.relativedelta(end_date.replace(day=1), start_date.replace(day=1))
+    period_by_months = list(map(make_date, range(r.months + 1)))
+    return list(map(make_data, period_by_months))
