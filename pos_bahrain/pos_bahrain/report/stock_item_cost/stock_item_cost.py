6ab55f3589c806da993fc50b4a15e1fd9ce32d38
Ivan Ray Altomera <irayspacii@gmail.com>
2020-11-17 22:42:14 +0800

    feat: gp details

--- pos_bahrain/pos_bahrain/report/stock_item_cost/stock_item_cost.py
+++ pos_bahrain/pos_bahrain/report/stock_item_cost/stock_item_cost.py
@@ -38,14 +38,11 @@ def _get_columns():
                 make_column("description", "Description", width=350),
                 make_column("barcode", "Barcode", width=180),
             ],
-            [
-                make_column(_get_key(x), x, "Float")
-                for x in _get_warehouses()
-            ],
+            [make_column(_get_key(x), x, "Float") for x in _get_warehouses()],
             [
                 make_column("qty_sold", "Qty Sold", "Float"),
                 make_column("sold_valuation", "Sold Valuation", "Currency"),
-            ]
+            ],
         )
     )
 
@@ -53,22 +50,45 @@ def _get_columns():
 def _get_data(filters):
     items = _get_items()
 
-    def get_merged_data(rows):
-        item_barcodes = _get_item_barcodes()
-        item_stocks = compose(
-            partial(valmap, lambda x: {_get_key(z.get('warehouse')): z.get('actual_qty') for z in x}),
-            partial(groupby, 'item_code'),
-            lambda: _get_item_stocks()
-        )()
-        items_sold = _get_items_sold(filters)
-        return compose(
-            list,
-            partial(map, lambda x: merge(x, {'qty_sold': items_sold.get(x.get('name'), 0.00)})),
-            partial(map, lambda x: merge(x, item_stocks.get(x.get('name'), {}))),
-            partial(map, lambda x: merge(x, {'barcode': item_barcodes.get(x.get('name'))}))
-        )(rows)
+    def get_gp_details():
+        from erpnext.accounts.report.gross_profit.gross_profit import execute
 
-    return get_merged_data(items)
+        gp_columns, gp_data = execute(
+            frappe._dict(merge(filters, {"group_by": "Item Code"}))
+        )
+        columns = list(map(_get_fieldname, gp_columns))
+
+        def get_column_data(column_name, row):
+            column_data = columns.index(column_name)
+            return row[column_data]
+
+        return {
+            get_column_data("Item Code", x): {
+                "qty_sold": get_column_data("Qty", x),
+                "sold_valuation": get_column_data("Valuation Rate", x),
+            }
+            for x in gp_data
+        }
+
+    gp_details = get_gp_details()
+    item_barcodes = _get_item_barcodes()
+    item_stocks = compose(
+        partial(
+            valmap,
+            lambda x: {_get_key(z.get("warehouse")): z.get("actual_qty") for z in x},
+        ),
+        partial(groupby, "item_code"),
+        lambda: _get_item_stocks(),
+    )()
+
+    data = compose(
+        list,
+        partial(map, lambda x: merge(x, gp_details.get(x.get("name"), {}))),
+        partial(map, lambda x: merge(x, item_stocks.get(x.get("name"), {}))),
+        partial(map, lambda x: merge(x, {"barcode": item_barcodes.get(x.get("name"))})),
+    )
+
+    return data(items)
 
 
 def _get_items():
@@ -87,43 +107,26 @@ def _get_items():
     )
 
 
-def _get_items_sold(filters):
-    items_sold = frappe.db.sql(
-        """
-            SELECT
-                item_code,
-                SUM(qty) as qty
-            FROM `tabSales Invoice Item` sii
-            INNER JOIN `tabSales Invoice` si ON si.name = sii.parent
-            WHERE si.docstatus = 1
-            AND si.posting_date BETWEEN %(from_date)s AND %(to_date)s
-            GROUP BY item_code
-        """,
-        filters,
-        as_dict=1
-    )
-    return {x.get('item_code'): x.get('qty') for x in items_sold}
-
-
 def _get_item_barcodes():
     item_barcodes = frappe.db.get_all("Item Barcode", fields=["parent", "barcode"])
     return {x.get("parent"): x.get("barcode") for x in item_barcodes}
 
 
 def _get_item_stocks():
-    return frappe.db.get_all(
-        "Bin",
-        fields=["warehouse", "item_code", "actual_qty"]
-    )
+    return frappe.db.get_all("Bin", fields=["warehouse", "item_code", "actual_qty"])
 
 
 def _get_warehouses():
     return compose(list, partial(pluck, "name"))(
-        frappe.db.get_all(
-            "Warehouse", filters={"is_group": 0}, fields=["name"]
-        )
+        frappe.db.get_all("Warehouse", filters={"is_group": 0}, fields=["name"])
     )
 
 
 def _get_key(text):
     return text.lower().replace(" ", "_").replace("-", "_")
+
+
+def _get_fieldname(column):
+    if isinstance(column, dict):
+        return column.get("label")
+    return column.split(":")[0]
