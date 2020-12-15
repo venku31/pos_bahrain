223e0e62c9b6667747f00c1c56a7950abd141cc6
Ivan Ray Altomera <irayspacii@gmail.com>
2020-12-15 19:11:55 +0800

    fix(withStockQty): undefined bin_qty

--- pos_bahrain/public/js/addons/withStockQty.js
+++ pos_bahrain/public/js/addons/withStockQty.js
@@ -7,9 +7,12 @@ export default function withStockQty(Pos) {
     _update_stock_qty() {
       const me = this;
       $.each(this.frm.doc.items || [], function (i, item) {
-        const bin_qty = me.bin_data[item.item_code][item.warehouse];
-        const qty = item.qty * item.conversion_factor;
-        me.bin_data[item.item_code][item.warehouse] = bin_qty - qty;
+        const bin_data = me.bin_data[item.item_code];
+        if (bin_data) {
+          const bin_qty = bin_data[item.warehouse];
+          const qty = item.qty * item.conversion_factor;
+          me.bin_data[item.item_code][item.warehouse] = bin_qty - qty;
+        }
       });
       this.items = this.get_items();
       this.make_item_list();
