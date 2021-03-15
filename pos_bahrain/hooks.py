30f02ada0fc59a9b67a9c9a68cfa13941ce1bf36
Ivan Ray Altomera <irayspacii@gmail.com>
2021-03-15 20:16:35 +0800

    feat: added repack request on Stock Entry

--- pos_bahrain/hooks.py
+++ pos_bahrain/hooks.py
@@ -148,6 +148,7 @@ fixtures = [
                     "Branch-pb_cr_no",
                     "Branch-pb_cr_expiry",
                     "Stock Entry-pb_reference_stock_transfer",
+                    "Stock Entry-pb_repack_request",
                 ],
             ]
         ],
