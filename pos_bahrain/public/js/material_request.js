d22ef2941a1e8161c56229f35e84612cda265603
Ivan Ray Altomera <irayspacii@gmail.com>
2021-03-18 22:25:23 +0800

    fix(material_request): Stock Transfer display even already Transferred

--- pos_bahrain/public/js/material_request.js
+++ pos_bahrain/public/js/material_request.js
@@ -1,6 +1,6 @@
-frappe.ui.form.on("Material Request", {
+frappe.ui.form.on('Material Request', {
   setup: function (frm) {
-    frm.set_query("pb_to_warehouse", function () {
+    frm.set_query('pb_to_warehouse', function () {
       return {
         filters: {
           company: frm.doc.company,
@@ -17,27 +17,35 @@ frappe.ui.form.on("Material Request", {
   },
 });
 
-
 function _make_custom_buttons(frm) {
   if (frm.doc.docstatus !== 1) {
     return;
   }
-  if (frm.doc.material_request_type === "Material Transfer" && frappe.user.has_role("Stock Manager")) {
-    frm.add_custom_button(__("Stock Transfer"), () => _make_stock_transfer(frm));
+  if (
+    frm.doc.material_request_type === 'Material Transfer' &&
+    frm.doc.status !== 'Transferred' &&
+    frappe.user.has_role('Stock Manager')
+  ) {
+    frm.add_custom_button(__('Stock Transfer'), () =>
+      _make_stock_transfer(frm)
+    );
   }
 }
 
-
 function _make_stock_transfer(frm) {
   frappe.model.open_mapped_doc({
-      method: "pos_bahrain.api.material_request.make_stock_entry",
-      frm: frm
+    method: 'pos_bahrain.api.material_request.make_stock_entry',
+    frm: frm,
   });
 }
 
-
 function _set_items_warehouse(frm) {
   for (const item of frm.doc.items) {
-    frappe.model.set_value(item.doctype, item.name, "warehouse", frm.doc.pb_to_warehouse);
+    frappe.model.set_value(
+      item.doctype,
+      item.name,
+      'warehouse',
+      frm.doc.pb_to_warehouse
+    );
   }
 }
