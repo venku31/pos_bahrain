c12f3720f9a86212477860d911a0179bc48d62ea
Ivan Ray Altomera <irayspacii@gmail.com>
2021-03-30 23:25:43 +0800

    feat(hooks): branch

--- pos_bahrain/hooks.py
+++ pos_bahrain/hooks.py
@@ -46,8 +46,13 @@ doctype_js = {
         "public/js/alternate_discount.js",
         "public/js/includes/scan_barcode.js",
         "public/js/price_list.js",
+        "public/js/branch.js",
     ],
-    "Sales Order": ["public/js/alternate_discount.js", "public/js/price_list.js"],
+    "Sales Order": [
+        "public/js/alternate_discount.js",
+        "public/js/price_list.js",
+        "public/js/branch.js",
+    ],
     "Purchase Invoice": [
         "public/js/set_retail_price.js",
         "public/js/includes/scan_barcode.js",
