d876aea5887316719554ca4d0d995ebf0483ea12
Ivan Ray Altomera <irayspacii@gmail.com>
2021-05-18 21:15:10 +0800

    fix(hide_sales_return): unhide if not ticked

--- pos_bahrain/public/js/scripts/extensions/hide_sales_return.js
+++ pos_bahrain/public/js/scripts/extensions/hide_sales_return.js
@@ -2,7 +2,7 @@ export default async function(name, group) {
   const { message: data } = await frappe.call({
     method: 'pos_bahrain.api.pos_bahrain_settings.hide_sales_return',
   });
-  if (!frappe.user.has_role(data)) {
+  if (data && !frappe.user.has_role(data)) {
     setTimeout(() => {
       const encoded_text = encodeURIComponent(name);
       const query = ".btn-group[data-label='" + group +"'] li > a[data-label='" + encoded_text + "']";
