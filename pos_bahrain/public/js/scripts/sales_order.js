f73383a4459a2a0935cfd3ecb7cf443fe6a77cbd
World Shading <worldsh@vmi487722.contaboserver.net>
2021-09-06 01:02:55 +0300

    fix: sync naming series on sales order

--- pos_bahrain/public/js/scripts/sales_order.js
+++ pos_bahrain/public/js/scripts/sales_order.js
@@ -5,9 +5,20 @@ const sales_order_item = {
   barcode: set_uom,
 };
 
+async function set_naming_series(frm) {
+  const { os_branch: branch } = frm.doc;
+  if (branch) {
+    const {
+      message: { os_sales_order_naming_series } = {},
+    } = await frappe.db.get_value('Branch', branch, 'os_sales_order_naming_series');
+    frm.set_value('naming_series', os_sales_order_naming_series);
+  }
+}
+
 export default {
   sales_order_item,
   setup: set_uom_query,
+  pb_branch: set_naming_series,
   refresh: function(frm) {
     if (frm.doc.__islocal) {
       set_fields(frm);
