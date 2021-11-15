dc7bf23aa019838c49ec035d774b21b5eed03df0
neeraj <neerajvkn@gmail.com>
2021-11-15 20:33:34 +0300

    added branch barcode print

---
+++ pos_bahrain/pos_bahrain/doctype/branch_barcode_print/branch_barcode_print.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2021, 	9t9it and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Branch Barcode Print', pos_bahrain.scripts.barcode_print);
+frappe.ui.form.on( 'Barcode Print Item', pos_bahrain.scripts.barcode_print.barcode_print_item );
