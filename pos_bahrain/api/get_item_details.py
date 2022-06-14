31d2a8ef1baf34f4b75a25195d83bc9cbb543223
venkatesh <venkatesh.n@indictranstech.com>
2022-06-14 17:13:28 +0530

    Enabled get_item_details in hooks for fetch items in quotation and SO

--- pos_bahrain/api/get_item_details.py
+++ pos_bahrain/api/get_item_details.py
@@ -9,15 +9,15 @@ from six import string_types
 def get_item_details(args):
     from erpnext.stock.get_item_details import get_item_details
 
-    # out = get_item_details(args)
-    # args_dict = (
-    #     frappe._dict(json.loads(args)) if isinstance(args, string_types) else args
-    # )
-    # default_warehouse = (
-    #     frappe.db.get_value("Company", args_dict.company, "default_warehouse")
-    #     if args_dict.company
-    #     else None
-    # )
-    # if default_warehouse and not out.warehouse:
-    #     out.warehouse = default_warehouse
-    # return out
+    out = get_item_details(args)
+    args_dict = (
+        frappe._dict(json.loads(args)) if isinstance(args, string_types) else args
+    )
+    default_warehouse = (
+        frappe.db.get_value("Company", args_dict.company, "default_warehouse")
+        if args_dict.company
+        else None
+    )
+    if default_warehouse and not out.warehouse:
+        out.warehouse = default_warehouse
+    return out
