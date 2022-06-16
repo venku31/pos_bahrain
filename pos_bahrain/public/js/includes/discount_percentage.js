abeaa82cd28db337dd1f5c714927897a99261121
venkatesh <venkatesh.n@indictranstech.com>
2022-06-16 11:19:49 +0530

    Customer Pricelist added in qtn sales order and sales Invoice

--- pos_bahrain/public/js/includes/discount_percentage.js
+++ pos_bahrain/public/js/includes/discount_percentage.js
@@ -36,3 +36,22 @@ function _apply_pb_discount_percentage_on_item_code(doctype) {
   'Sales Order Item',
   'Sales Invoice Item',
 ].forEach((doctype) => _apply_pb_discount_percentage_on_item_code(doctype));
+
+function _apply_pricelist(doctype) {
+  frappe.ui.form.on(doctype, {
+    apply_offer: function (frm) {
+      frm.doc.items.forEach(({ doctype, name }) => {
+        frappe.model.set_value(
+          doctype,
+          name,
+          'pb_price_list',
+          frm.doc.customer_price_list
+        );
+      });
+    },
+  });
+}
+['Quotation', 'Sales Order', 'Sales Invoice'].forEach((doctype) =>
+_apply_pricelist(doctype)
+);
+
