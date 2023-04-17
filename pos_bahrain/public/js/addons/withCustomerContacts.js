1047348bc4d980a1836c2e2db04ecccb9571b813
Chipo <chipohameja@gmail.com>
2023-04-17 09:48:11 +0000

    fix: fetch address line1m line2 and phone

--- pos_bahrain/public/js/addons/withCustomerContacts.js
+++ pos_bahrain/public/js/addons/withCustomerContacts.js
@@ -1,18 +1,43 @@
 export default function withCustomerContacts(Pos) {
-    return class PosExtended extends Pos {
-      async init_master_data(r, freeze) {
-        const pos_data = await super.init_master_data(r, freeze);
-        const { customer_contacts } = pos_data;
-        this.customer_contacts = customer_contacts;
-        return pos_data;
-      }
-      make_customer() {
-        super.make_customer();
-        const me = this;
-        me.party_field.$input.on('awesomplete-select', function (e) {
-          const customer = e.originalEvent.text.value;
-          me.frm.doc.phone = me.customer_contacts[customer] || '';
+  return class PosExtended extends Pos {
+    async init_master_data(r, freeze) {
+      const pos_data = await super.init_master_data(r, freeze);
+      const { customer_contacts } = pos_data;
+      this.customer_contacts = customer_contacts;
+      return pos_data;
+    }
+    make_customer() {
+      super.make_customer();
+      const me = this;
+      me.party_field.$input.on('awesomplete-select', function (e) {
+        console.log(e.originalEvent.text.value);
+
+        //Fetch address line 1 and 2
+        frappe.call({
+          method: "pos_bahrain.api.get_customer_details.get_customer_address",
+          args: {
+            customer_name: e.originalEvent.text.value
+          },
+          callback: (r) => {
+            localStorage.setItem("address_line1", r.message.address_line1);
+            localStorage.setItem("address_line2", r.message.address_line2);
+          }
         });
-      }
-    };
-  }
+
+        //Fetch contact
+        frappe.call({
+          method: "pos_bahrain.api.get_customer_details.get_customer_contact",
+          args: {
+            customer_name: e.originalEvent.text.value
+          },
+          callback: (r) => {
+            localStorage.setItem("contact", r.message.contact);
+          }
+        });
+
+        const customer = e.originalEvent.text.value;
+        me.frm.doc.phone = me.customer_contacts[customer] || '';
+      });
+    }
+  };
+}
