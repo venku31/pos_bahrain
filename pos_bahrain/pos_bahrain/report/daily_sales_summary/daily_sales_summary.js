fcd2e1589eae01a647f21f385cd04917bedbdc05
Chipo Hameja <chipohameja@gmail.com>
2023-05-13 11:49:39 +0000

    Added company filter on daily sales register

--- pos_bahrain/pos_bahrain/report/daily_sales_summary/daily_sales_summary.js
+++ pos_bahrain/pos_bahrain/report/daily_sales_summary/daily_sales_summary.js
@@ -19,5 +19,12 @@ frappe.query_reports['Daily Sales Summary'] = {
       fieldtype: 'Date',
       default: frappe.datetime.get_today(),
     },
+    {
+      fieldname: 'company',
+      label: __('Company'),
+      fieldtype: 'Link',
+      options: 'Company',
+      default: frappe.defaults.get_user_default('Company')
+    }
   ],
 };
