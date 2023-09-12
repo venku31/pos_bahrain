72bec2e405d3081b63405b253e10bb4dc0bbadaa
bhavesh <bhaveshmtpl>
2023-09-12 19:07:44 +0530

    Purchase Invoice/Receipt not allow returing expired batch

--- pos_bahrain/core_overrides/stock_ledger_entry.py
+++ pos_bahrain/core_overrides/stock_ledger_entry.py
@@ -11,12 +11,15 @@ from erpnext.stock.doctype.stock_ledger_entry.stock_ledger_entry import StockLed
 def _validate_batch(self):
     if self.batch_no and self.voucher_type != 'Stock Entry':
         purchase_return = _get_purchase_return(self.voucher_type, self.voucher_no)
-        if not purchase_return:
-            expiry_date = frappe.db.get_value('Batch', self.batch_no, 'expiry_date')
-            if expiry_date:
-                if getdate(self.posting_date) > getdate(expiry_date):
-                    pass
-                    # frappe.throw(_('Batch {0} of Item {1} has expired.').format(self.batch_no, self.item_code))
+        is_return = _get_purchase_return(self.voucher_type, self.voucher_no)
+        if self.voucher_type in ['Purchase Invoice', 'Purchase Receipt'] and is_return:
+            pass 
+        else:
+            if not purchase_return:
+                expiry_date = frappe.db.get_value('Batch', self.batch_no, 'expiry_date')
+                if expiry_date:
+                    if getdate(self.posting_date) > getdate(expiry_date):
+                        frappe.throw(_('Batch {0} of Item {1} has expired.').format(self.batch_no, self.item_code))
 
 
 def _get_purchase_return(voucher_type, voucher_no):
