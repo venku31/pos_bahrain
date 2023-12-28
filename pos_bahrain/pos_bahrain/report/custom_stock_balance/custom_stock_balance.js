8a858449214c1361ee628b04f00fd847845d1be6
Bhavesh Midocean <bhaveshmtpl@gmail.com>
2023-12-28 17:03:07 +0530

    remove test report

--- pos_bahrain/pos_bahrain/report/custom_stock_balance/custom_stock_balance.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2016, 	9t9it and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["Custom Stock Balance"] = {
-	"filters": [
-
-	]
-};
